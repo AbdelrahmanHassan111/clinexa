a760cb8ee887604eb635801dbb15f37506994874
Abdelrahman Hassan <156480367+AbdelrahmanHassan111@users.noreply.github.com>
2025-05-11 02:37:13 +0300

    patient_portal.py

--- streamlit_app/patient_portal.py
+++ streamlit_app/patient_portal.py
@@ -17,12 +17,12 @@ try:
 except ImportError:
     # Fallback configuration if db_config.py is not available
     DB_CONFIG = {
-        "host": "sql.freedb.tech",
-        "port": 3306,
-        "user": "freedb_clinexa",
-        "password": "PJ%g2KQX&J7vW*4",
-        "database": "freedb_clinexa"
-    }
+    "host": st.secrets["connections"]["mysql"]["host"],
+    "port": st.secrets["connections"]["mysql"]["port"],
+    "user": st.secrets["connections"]["mysql"]["username"],
+    "password": st.secrets["connections"]["mysql"]["password"],
+    "database": st.secrets["connections"]["mysql"]["database"]
+}
 
 # Add custom CSS for better UI
 st.markdown("""
