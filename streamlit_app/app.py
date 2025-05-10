d2869db90edc9e00ec5c95129f13b81301eeef6f
Abdelrahman Hassan <156480367+AbdelrahmanHassan111@users.noreply.github.com>
2025-05-10 17:52:39 +0300

    app.py

--- streamlit_app/app.py
+++ streamlit_app/app.py
@@ -16,11 +16,11 @@ try:
 except ImportError:
     # Fallback configuration if db_config.py is not available
     DB_CONFIG = {
-        "host": "sql.freedb.tech",
-        "port": 3306,
-        "user": "freedb_clinexa",
-        "password": "PJ%g2KQX&J7vW*4",
-        "database": "freedb_clinexa"
+        "host": st.secrets["connections"]["mysql"]["host"],
+    "port": st.secrets["connections"]["mysql"]["port"],
+    "user": st.secrets["connections"]["mysql"]["username"],
+    "password": st.secrets["connections"]["mysql"]["password"],
+    "database": st.secrets["connections"]["mysql"]["database"]
     }
 
 def get_db_connection():
